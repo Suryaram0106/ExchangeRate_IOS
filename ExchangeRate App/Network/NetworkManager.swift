a01dd48cd525eb9e61565e43a4bbd6e468008c94
Surya <surya@Suryas-MacBook-Pro.local>
2025-06-18 15:00:07 +0530

    second commit

--- ExchangeRate App/Network/NetworkManager.swift
+++ ExchangeRate App/Network/NetworkManager.swift
@@ -1,6 +1,51 @@
+//import Foundation
+//
+//class NetworkManager {
+//    static let shared = NetworkManager()
+//    private init() {}
+//
+//    private let baseURL = "https://v6.exchangerate-api.com/v6/0601664db3e6cb1c57b2e7c4/latest/USD"
+//
+//    //private let baseURL = "http://api.exchangeratesapi.io/v1/latest?access_key=0353337fec4fa397a71d253769676291"
+//    
+//    func fetchExchangeRates(completion: @escaping (Result<ExchangeRateResponse, Error>) -> Void) {
+//        guard let url = URL(string: baseURL) else {
+//            completion(.failure(NSError(domain: "Invalid URL", code: -1)))
+//            return
+//        }
+//
+//        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+//            if let error = error {
+//                completion(.failure(error))
+//                return
+//            }
+//
+//            guard let data = data else {
+//                completion(.failure(NSError(domain: "No data", code: -1)))
+//                return
+//            }
+//
+//            do {
+//                let result = try JSONDecoder().decode(ExchangeRateResponse.self, from: data)
+//                print("✅ API Response: \(result)")
+//                print("🔁 Conversion Rates: \(result.conversion_rates)")
+//                completion(.success(result))
+//            } catch {
+//                print("❌ Decoding error: \(error)")
+//                completion(.failure(error))
+//            }
+//        }
+//
+//        task.resume()
+//    }
+//}
+
 import Foundation
 
-class NetworkManager {
+protocol ExchangeRateServiceProtocol {
+    func fetchExchangeRates(completion: @escaping (Result<ExchangeRateResponse, Error>) -> Void)
+}
+class NetworkManager: ExchangeRateServiceProtocol {
     static let shared = NetworkManager()
     private init() {}
 
@@ -34,3 +79,6 @@ class NetworkManager {
         task.resume()
     }
 }
+
+
+
--- ExchangeRate App/ViewModel/ExchangeRateViewModel.swift
+++ ExchangeRate App/ViewModel/ExchangeRateViewModel.swift
@@ -1,20 +1,56 @@
+//import Foundation
+//import Combine
+//
+//class ExchangeRateViewModel: ObservableObject {
+//    @Published var rates: [String: Double] = [:]
+//    @Published var isLoading = false
+//    @Published var errorMessage: String?
+//
+//    
+//    
+//    func loadRates() {
+//        isLoading = true
+//        errorMessage = nil
+//
+//        NetworkManager.shared.fetchExchangeRates { [weak self] result in
+//            DispatchQueue.main.async {
+//                self?.isLoading = false
+//                switch result {
+//                case .success(let response):
+//                    self?.rates = response.conversion_rates
+//                case .failure(let error):
+//                    self?.errorMessage = error.localizedDescription
+//                }
+//            }
+//        }
+//    }
+//    
+//    
+//
+//}
+
 import Foundation
 import Combine
 
+//protocol ExchangeRateServiceProtocol {
+//    func fetchExchangeRates(completion: @escaping (Result<ExchangeRateResponse, Error>) -> Void)
+//}
 class ExchangeRateViewModel: ObservableObject {
     @Published var rates: [String: Double] = [:]
     @Published var isLoading = false
     @Published var errorMessage: String?
 
-    
+    private let service: ExchangeRateServiceProtocol
 
-
-    
+    init(service: ExchangeRateServiceProtocol = NetworkManager.shared) {
+           self.service = service
+       }
+        
     func loadRates() {
         isLoading = true
         errorMessage = nil
 
-        NetworkManager.shared.fetchExchangeRates { [weak self] result in
+        service.fetchExchangeRates { [weak self] result in
             DispatchQueue.main.async {
                 self?.isLoading = false
                 switch result {
@@ -26,6 +62,11 @@ class ExchangeRateViewModel: ObservableObject {
             }
         }
     }
+    
+
+    
+    
+
     
     
 
